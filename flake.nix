dbc0d8ca1e2973c6d9c066775cc0027f2f861462
ouzu <git@ouzu.tech>
2023-09-15 20:56:42 +0200

    unify rootfs

--- flake.nix
+++ flake.nix
@@ -6,28 +6,27 @@
 
   outputs = { self, nixpkgs, microvm, ... } @ allAttrs:
     let
-      numOfFogNodes = allAttrs.numOfFogNodes or 1;
-      numOfEdgeNodesPerFog = allAttrs.numOfEdgeNodesPerFog or  1;
+      numOfFogNodes = allAttrs.numOfFogNodes or 3;
+      numOfEdgeNodesPerFog = allAttrs.numOfEdgeNodesPerFog or 3;
 
       genFogNodes = builtins.map
         (n: {
           name = "fog";
           id = n + 2;
-          parent = 2;
         })
         (lib.range 1 numOfFogNodes);
 
-      genEdgeNodes = lib.flatten (builtins.map
-        (fog: builtins.map
-          (n: {
-            name = "edge";
-            id = (3 + numOfFogNodes) + ((fog.id - 4) * numOfEdgeNodesPerFog) + n;
-            parent = fog.id;
-          })
-          (lib.range 1 numOfEdgeNodesPerFog))
-        genFogNodes);
+      genEdgeNodes = builtins.map
+        (n: {
+          name = "edge";
+          id = n + 2 + numOfFogNodes;
+        })
+        (lib.range 1 (numOfFogNodes * numOfEdgeNodesPerFog));
 
-      vmData = [{ name = "cloud"; id = 2; parent = null; }] ++ genFogNodes ++ genEdgeNodes;
+      vmData = [{
+        name = "cloud";
+        id = 2;
+      }] ++ genFogNodes ++ genEdgeNodes;
 
       networkProperties = {
         cloud = {
@@ -50,6 +49,16 @@
 
       lib = pkgs.lib;
       mod = lib.mod;
+
+      fillLeadingZeros = s:
+        let
+          len = builtins.stringLength s;
+        in
+        if len == 0
+        then "00"
+        else if len == 1
+        then "0${s}"
+        else s;
 
       decToHex =
         with lib; let
@@ -83,23 +92,85 @@
         let
           hexValue = toHex' v "";
         in
-        if builtins.stringLength hexValue == 0 then
-          "00"
-        else if builtins.stringLength hexValue == 1 then
-          "0" + hexValue
-        else
-          hexValue;
+        fillLeadingZeros hexValue;
 
-      generateVM = { name, id, parent }: {
+      networkSetupScript = pkgs.writeShellScript "setup-networking.sh" ''
+          #!/usr/bin/env bash
+
+          set -e
+
+          log() {
+              echo "[$(date)] $1" | tee -a /var/log/setup-networking.log
+          }
+
+          PATH=$PATH:${lib.makeBinPath (with pkgs; [ iproute2 gawk ])}
+
+          MAC_ADDR=$(ip link show dev eth0 | grep ether | awk '{print $2}')
+          IFS=':' read -ra ADDR <<< "$MAC_ADDR"
+          NUM_OF_FOG_NODES=''${ADDR[2]}
+          NUM_OF_EDGE_NODES_PER_FOG=''${ADDR[3]}
+          NODE_ID=$((0x''${ADDR[4]}''${ADDR[5]}))
+          if [[ "$NODE_ID" -eq 2 ]]; then
+              ROLE="cloud"
+          elif [[ "$NODE_ID" -gt 2 && "$NODE_ID" -le $((2 + 0x$NUM_OF_FOG_NODES)) ]]; then
+              ROLE="fog"
+          else
+              ROLE="edge"
+          fi
+          HOSTNAME="''${ROLE}-''${NODE_ID}"
+          IPV4_ADDR="172.20.$((NODE_ID / 256)).$((NODE_ID % 256))"
+          DEFAULT_GATEWAY="172.20.0.1"
+
+          log "MAC_ADDR: $MAC_ADDR"
+          log "NUM_OF_FOG_NODES: $NUM_OF_FOG_NODES"
+          log "NUM_OF_EDGE_NODES_PER_FOG: $NUM_OF_EDGE_NODES_PER_FOG"
+          log "NODE_ID: $NODE_ID"
+          log "ROLE: $ROLE"
+          log "HOSTNAME: $HOSTNAME"
+          log "IPV4_ADDR: $IPV4_ADDR"
+
+          ip addr flush dev eth0
+          ip addr add $IPV4_ADDR/24 dev eth0
+          ip link set eth0 up
+          #echo "$HOSTNAME" > /etc/hostname
+          #hostname "$HOSTNAME"
+          ip route add default via $DEFAULT_GATEWAY
+
+          mkdir -p /var/lib/mistify
+
+          cat > /var/lib/mistify/config.toml <<EOF
+        ConfigPort = 8080
+        RProxyConfigPort = 8081
+        RProxyListenAddress = "0.0.0.0"
+        RProxyBin = "./rproxy"
+
+        COAPPort = 5683
+        HTTPPort = 80
+        GRPCPort = 9000
+
+        Backend = "docker"
+        ID = "$HOSTNAME"
+
+        Mode = $ROLE
+
+        RegistryPort = 8082
+        Host = "$IPV4_ADDR"
+        EOF
+        
+          if [[ "$ROLE" == "fog" ]]; then
+              echo "ParentAddress = \"172.20.0.2\"" >> /var/lib/mistify/config.toml
+          elif [[ "$ROLE" == "edge" ]]; then
+              PARENT_ID=$(( (NODE_ID - 2 - 1) / NUM_OF_EDGE_NODES_PER_FOG + 2 ))
+              PARENT_FOG_NODE_IP="172.20.$((PARENT_ID / 256)).$((PARENT_ID % 256))"
+              echo "ParentAddress = \"$PARENT_FOG_NODE_IP\"" >> /var/lib/mistify/config.toml
+          fi
+      '';
+
+      generateVM = { name, id }: {
         users.users.root.password = "";
         environment.systemPackages = with pkgs; [
           iperf
-          tcpdump
           iproute2
-          traceroute
-          netcat
-          socat
-          iputils
           nmap
         ];
 
@@ -116,25 +187,37 @@
         };
 
         networking = {
-          hostName = "${name}-${builtins.toString id}";
+          hostName = "vm";
           useDHCP = false;
           interfaces."eth0".ipv4.addresses = [{
-            address = "172.20.${builtins.toString (id / 256)}.${builtins.toString (mod id 256)}";
+            address = "172.21.0.1";
             prefixLength = 16;
           }];
           defaultGateway = "172.20.0.1";
         };
 
+        systemd.services.setupNetwork = {
+          description = "Setup networking based on MAC address";
+          after = [ "network-pre.target" ];
+          before = [ "sshd.service" ];
+          wantedBy = [ "multi-user.target" ];
+          serviceConfig = {
+            Type = "oneshot";
+            RemainAfterExit = "yes";
+            ExecStart = "${networkSetupScript}";
+          };
+        };
+
         microvm = {
-          volumes = [{
-            mountPoint = "/var";
-            image = "var.img";
-            size = 256;
-          }];
           interfaces = [{
             type = "tap";
             id = "tap-${name}-${builtins.toString id}";
-            mac = "02:00:00:00:${decToHex (mod (id / 256) 2560)}:${decToHex (mod id 256)}";
+            mac =
+              let
+                nFog = fillLeadingZeros (builtins.toString numOfFogNodes);
+                nEdge = fillLeadingZeros (builtins.toString numOfEdgeNodesPerFog);
+              in
+              "02:00:${builtins.substring 0 2 nFog}:${builtins.substring 0 2 nEdge}:${decToHex (mod (id / 256) 256)}:${decToHex (mod id 256)}";
           }];
           socket = "/tmp/${builtins.toString id}-${name}.socket";
           hypervisor = "firecracker";
@@ -236,7 +319,6 @@
 
       teardownScript = mkScript ''
         #!/usr/bin/env bash
-        set -e
 
         teardown_networking() {
             echo "Tearing down the network..."
@@ -269,6 +351,21 @@
         done
       '';
 
+      mapVMData = builtins.map
+        (vm: {
+          inherit vm;
+          divResult = toString (builtins.div vm.id 256);
+          modResult = toString (lib.mod vm.id 256);
+        })
+        vmData;
+
+
+      stopVMsScript = mkScript ''
+        #!/usr/bin/env bash
+
+        killall firecracker
+      '';
+
     in
     {
       nixosConfigurations = generateVMs vmData;
@@ -283,9 +380,13 @@
           type = "app";
           program = "${teardownScript}/bin/script";
         };
-        startVMs = {
+        start = {
           type = "app";
           program = "${startVMsScript}/bin/script";
+        };
+        stop = {
+          type = "app";
+          program = "${stopVMsScript}/bin/script";
         };
       };
     };
