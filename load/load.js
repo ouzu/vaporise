b4fe1642cdb6ca63c4f91d8a3a73162d551ce0d1
ouzu <git@ouzu.tech>
2023-10-04 10:59:34 +0200

    update load

--- load/load.js
+++ load/load.js
@@ -4,33 +4,42 @@ import { Counter } from "k6/metrics";
 
 let ErrorCount = new Counter("errors");
 
+const N_FOG = 16;
+const N_EDGE_PER_FOG = 6;
+let ALL_EDGE_NODES = false;
+let IP_SKIP = ALL_EDGE_NODES ? 1 : N_EDGE_PER_FOG;
+
+const START_NODE = 3 + N_FOG;
+const END_NODE = START_NODE + N_EDGE_PER_FOG * N_FOG - 1;
+
+const VUS_PER_EDGE_REGION = 10;
+
+const TOTAL_VUS = VUS_PER_EDGE_REGION * N_FOG;
+
 export let options = {
   stages: [
-    { duration: "15s", target: 30, rate: "10/s" },
-    { duration: "2m", target: 30, rate: "10/s" },
+    { duration: "15s", target: TOTAL_VUS },
+    { duration: "2m", target: TOTAL_VUS },
     { duration: "15s", target: 0 },
   ],
 };
 
-// Choose the range of IP addresses
-const START_IP = 6;
-const END_IP = 14;
 
-// Helper function to get random IP in the range
-function getRandomIP() {
-  return `172.20.0.${Math.floor(
-    Math.random() * (END_IP - START_IP + 1) + START_IP
-  )}`;
+function getIP(vu) {
+  let totalIPs = Math.floor((END_NODE - START_NODE + 1) / IP_SKIP);
+  let nodeIndex = (vu-1) % totalIPs;
+  return `172.20.0.${START_NODE + (nodeIndex * IP_SKIP)}`;
 }
 
 export default function () {
-  let ip = getRandomIP();
+  let ip = getIP(__VU);
   let url = `http://${ip}/matmul`;
 
   let params = {
     headers: {
       "Content-Type": "application/json",
     },
+    timeout: 120000,  
   };
 
   let payload = JSON.stringify({
@@ -48,5 +57,5 @@ export default function () {
     ErrorCount.add(1);
   }
 
-  sleep(1); // Adjust sleep as needed
+  sleep(1); 
 }
